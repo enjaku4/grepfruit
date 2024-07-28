e4462d8a05bfb79ea0b134f57bd07c9649aadaae
enjaku4 <enjaku4@gmail.com>
2024-07-28 04:31:48 +0200

    fix tests

--- spec/grepfruit_spec.rb
+++ spec/grepfruit_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Grepfruit do
     subject { `./exe/grepfruit` }
 
     it { is_expected.to include("Searching for /TODO/...") }
-    it { is_expected.to include("README.md:29") }
+    it { is_expected.to include("README.md:31") }
     it { is_expected.to include("Search for the pattern `TODO` in the current directory, excluding the default directories:") }
     it { is_expected.to include("18 files checked") }
     it { is_expected.to include("6 matches found") }
@@ -23,7 +23,7 @@ RSpec.describe Grepfruit do
   end
 
   context "when regex is specified" do
-    subject { `./exe/grepfruit -r 'opts|spec'` }
+    subject { `./exe/grepfruit -r 'opts|spec' -e vendor` }
 
     it { is_expected.to include("Searching for /opts|spec/...") }
     it { is_expected.to include("grepfruit.gemspec:5") }
@@ -32,7 +32,7 @@ RSpec.describe Grepfruit do
   end
 
   context "when multiple directories and files are excluded" do
-    subject { `./exe/grepfruit -e 'exe,spec,README.md'` }
+    subject { `./exe/grepfruit -e 'exe,spec,README.md,vendor'` }
 
     it { is_expected.not_to include("exe/grepfruit") }
     it { is_expected.not_to include("README.md") }
@@ -40,14 +40,14 @@ RSpec.describe Grepfruit do
   end
 
   context "when a relative path is specified" do
-    subject { `./exe/grepfruit -e 'exe/grepfruit'` }
+    subject { `./exe/grepfruit -e 'exe/grepfruit,vendor'` }
 
     it { is_expected.not_to include("exe/grepfruit") }
     it { is_expected.to include("5 matches found") }
   end
 
   context "when only a part of the file name is excluded" do
-    subject { `./exe/grepfruit -e 'fruit'` }
+    subject { `./exe/grepfruit -e 'fruit,vendor'` }
 
     it { is_expected.to include("exe/grepfruit") }
   end
